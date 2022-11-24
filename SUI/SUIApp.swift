0c63f05522ab21ead57a84c6d49683b0ed12be6c
qiyu <qiyu56@qq.com>
2022-11-24 10:22:12 +0800

    Initial Commit

---
+++ SUI/SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUIApp.swift
+//  SUI
+//
+//  Created by Yu Qi on 2022/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
